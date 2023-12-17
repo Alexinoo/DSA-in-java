12bab16cccd23c0d807b86cd1f9482b20c944190
Alex Mwangi <alex@stl-horizon.com>
2023-12-17 22:27:06 +0300

    prepend() method

---
+++ src/linkedlist/Prepend.java
@@ -0,0 +1,14 @@
+package linkedlist;
+
+public class Prepend {
+
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(2);
+
+        // Prepend 1 :Result - 1 2
+         myLinkedList.prepend(1);
+
+        myLinkedList.printList();
+
+    }
+}

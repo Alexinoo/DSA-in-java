c6112af7e9e2a49145503dabc2aaafe01f1c9730
Alex Mwangi <alex@stl-horizon.com>
2023-12-17 22:31:20 +0300

    get(int index) - Returns Node at a particular index

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -83,6 +83,15 @@ public class LinkedList {
         return temp;
     }
 
+    public Node get(int index){
+        if(index < 0 || index >= length) return null;
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
     public void getHead() {
         System.out.println("Head : "+head.value);
     }

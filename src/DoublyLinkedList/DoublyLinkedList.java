33d7ed72a683382c2364361d2d25ec70758d3ca9
Alex Mwangi <alex@stl-horizon.com>
2023-12-19 17:33:04 +0300

    DLL removeLast()

--- src/DoublyLinkedList/DoublyLinkedList.java
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -36,6 +36,22 @@ public class DoublyLinkedList {
     }
 
 
+    public Node removeLast() {
+        if (length == 0)  return null;
+
+        Node temp = tail;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        }else {
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length--;
+        return temp;
+    }
+
 
     public void printList(){
         Node temp = head;

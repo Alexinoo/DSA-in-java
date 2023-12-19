76245a06900ec5435f002ef4ab5b05f3b96287d5
Alex Mwangi <alex@stl-horizon.com>
2023-12-19 10:27:33 +0300

    Insert(index,value) - Insert value at a specified index

---
+++ src/linkedlist/Insert.java
@@ -0,0 +1,31 @@
+package linkedlist;
+
+public class Insert {
+    public static void main(String[] args){
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(3);
+
+        /* Insert Edge cases
+        *  - Insert at the beginning ( When index == 0 )
+        *  - Insert at the middle - More logic
+        *  - Insert at the end ( When index == lengthOFTheList )
+        *
+        */
+
+        System.out.println("Before Insert");
+        myLinkedList.printList();
+
+        /* Insert 1 at the beginning */
+        myLinkedList.insert(0,0);
+
+
+        /* Insert 2 at the middle */
+        myLinkedList.insert(2,2);
+
+        /* Insert 3 at the end */
+        myLinkedList.insert(4,4);
+
+        System.out.println("After Insert");
+        myLinkedList.printList();
+    }
+}

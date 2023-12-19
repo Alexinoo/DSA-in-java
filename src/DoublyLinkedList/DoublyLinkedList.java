2f567861610684327861751811b88cc10d1a871b
Alex Mwangi <alex@stl-horizon.com>
2023-12-19 14:51:11 +0300

    DLL Append()

--- src/DoublyLinkedList/DoublyLinkedList.java
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -22,6 +22,19 @@ public class DoublyLinkedList {
         length = 1;
     }
 
+    public void append(int value){
+        Node newNode = new Node(value);
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
 
 
     public void printList(){

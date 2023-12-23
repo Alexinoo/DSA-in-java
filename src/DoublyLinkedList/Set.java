a4650147411b8800ef3083f4e41073885743c312
Alex Mwangi <alex@stl-horizon.com>
2023-12-23 11:51:33 +0300

    DLL - Set(index, value)  Update a Node value at a specified index

---
+++ src/DoublyLinkedList/Set.java
@@ -0,0 +1,19 @@
+package DoublyLinkedList;
+
+public class Set {
+    public static void main(String[] args){
+        DoublyLinkedList myDLL = new DoublyLinkedList(11);
+        myDLL.append(3);
+        myDLL.append(23);
+        myDLL.append(7);
+
+        System.out.println("Before Update");
+        myDLL.printList();
+
+        // Update 3 where index is 1
+        myDLL.set(1,4);
+
+        System.out.println("After Update");
+        myDLL.printList();
+    }
+}

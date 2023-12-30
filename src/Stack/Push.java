9681e820faf894454c31160318fd13d715859fb5
Alex Mwangi <alex@stl-horizon.com>
2023-12-30 11:36:21 +0300

    Stack - Push() - prepends a node at the top of the stack

---
+++ src/Stack/Push.java
@@ -0,0 +1,13 @@
+package Stack;
+
+public class Push {
+
+    public static void main(String[] args){
+        Stack myStack = new Stack(2);
+
+        myStack.push(1);
+
+        myStack.printStack();
+
+    }
+}

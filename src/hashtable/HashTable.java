b5a9fdacb681bbae185c6cc0f73103565edee75d
Alex Mwangi <alex@stl-horizon.com>
2024-01-01 06:21:36 +0300

    HashTable - Hash() - Takes a key & generates unique address from 0-6 depending on size of an array

--- src/hashtable/HashTable.java
+++ src/hashtable/HashTable.java
@@ -19,6 +19,17 @@ public class HashTable {
         dataMap = new Node[size];
     }
 
+    private int Hash(String key){ //key = paint
+        int hash = 0;
+        char[] keyChars = key.toCharArray(); // ['p','a','i','n','t']
+
+        for(int i = 0; i < keyChars.length; i++) {
+            int asciiValue = keyChars[i]; //'p'
+            hash = (hash + asciiValue * 23) % dataMap.length; // (0 + 112*23) % 7 - Generate [0-6]
+        }
+        return hash;
+    }
+
     public void printTable(){
         for (int i = 0; i < dataMap.length; i++) {
             System.out.println(i + ":");

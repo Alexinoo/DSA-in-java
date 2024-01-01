3bbd8504fd6bbc87b1aca6da183b14e37bf66a84
Alex Mwangi <alex@stl-horizon.com>
2024-01-01 08:04:26 +0300

    HashTable - keys() - Returns all the keys in the datamap as an ArrayList

--- src/hashtable/HashTable.java
+++ src/hashtable/HashTable.java
@@ -1,5 +1,7 @@
 package hashtable;
 
+import java.util.ArrayList;
+
 public class HashTable {
     private int size=7;
     private Node[] dataMap;
@@ -52,6 +54,18 @@ public class HashTable {
              temp = temp.next;
         }
         return 0;  // Returns 0 if no match was found
+    }
+
+    public ArrayList keys(){
+        ArrayList<String> allKeys = new ArrayList<>();
+        for (int i = 0; i < dataMap.length; i++) {
+            Node temp = dataMap[i];
+            while(temp != null){
+                allKeys.add(temp.key);
+                temp = temp.next;
+            }
+        }
+        return allKeys;
     }
 
     public void printTable(){

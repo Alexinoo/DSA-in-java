4d30d8349f1406f93ae05b602cb3ddd135281a46
Alex Mwangi <alex@stl-horizon.com>
2024-01-02 06:42:20 +0300

    Graph - RemoveEdge class - main()

---
+++ src/graph/RemoveEdge.java
@@ -0,0 +1,27 @@
+package graph;
+
+public class RemoveEdge {
+    public static void main(String[] args){
+        Graph myGraph = new Graph();
+
+        //Add Vertex {A=[], B=[], C=[]}
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+        myGraph.printGraph();
+
+        //Add Edges {A=[B, C], B=[A, C], C=[A, B]}
+        myGraph.addEdge("A","B");
+        myGraph.addEdge("A","C");
+        myGraph.addEdge("B","C");
+        myGraph.printGraph();
+
+        //Remove Edge Between A and B vertices {A=[C], B=[C], C=[A, B]}
+        myGraph.removeEdge("A","B");
+        myGraph.printGraph();
+
+
+
+
+    }
+}

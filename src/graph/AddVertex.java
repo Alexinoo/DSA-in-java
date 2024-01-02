b8f6c4bd2168ed9fadb9da3ad1c0e179b5df37dc
Alex Mwangi <alex@stl-horizon.com>
2024-01-02 06:26:20 +0300

    Graph - AddVertex Class - main()

---
+++ src/graph/AddVertex.java
@@ -0,0 +1,9 @@
+package graph;
+
+public class AddVertex {
+    public static void main(String[] args){
+        Graph myGraph = new Graph();
+         myGraph.addVertex("A");
+        myGraph.printGraph();
+    }
+}

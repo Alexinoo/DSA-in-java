4f89ac9633ce08defbfc78fe3e8bebf80014b64a
Alex Mwangi <alex@stl-horizon.com>
2024-01-03 06:56:12 +0300

    Heap - Added sinkDown() - Bubbles down and swap value if greater than the parent

--- src/heap/Heap.java
+++ src/heap/Heap.java
@@ -42,4 +42,43 @@ public class Heap {
             current = parent(current);      //update current
         }
     }
+
+    public Integer remove(){
+        if(heap.size() == 0){
+            return null;
+        }
+
+        if(heap.size() == 1){
+            return heap.remove(0);
+        }
+
+        int maxValue = heap.get(0);
+        int lastIndex = heap.size() - 1;
+        heap.set(0,heap.remove(lastIndex));
+        sinkDown(0);
+        return maxValue;
+    }
+
+    private void sinkDown(int index){
+      int maxIndex = index;
+      while(true){
+          int leftIndex = leftChild(index);
+          int rightIndex = rightChild(index);
+
+          if(leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)){
+              maxIndex = leftIndex;
+          }
+
+          if(rightIndex < heap.size() && heap.get(rightIndex) > heap.get(maxIndex)){
+              maxIndex = rightIndex;
+          }
+
+          if(maxIndex != index){
+              swap(index,maxIndex);
+              index = maxIndex;
+          }else{
+              return;
+          }
+      }
+    }
 }

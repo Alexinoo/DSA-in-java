f9e14c1df5fd5426d73a177c502cf7c79ad7bc2b
Alex Mwangi <alex@stl-horizon.com>
2024-01-06 05:42:00 +0300

    rContains() - Written recursively with BST

--- src/binarysearchtree/BinarySearchTree.java
+++ src/binarysearchtree/BinarySearchTree.java
@@ -103,4 +103,26 @@ public class BinarySearchTree {
         }
         return false;
     }
+
+    /*
+    * Calling contains() recursively
+    * */
+
+    public boolean rContains(Node currentNode, int value){
+        if(currentNode == null) return false;
+
+        if(currentNode.value == value) return true;
+
+        if(value < currentNode.value){
+            return rContains(currentNode.left,value);
+        }else{
+            return rContains(currentNode.right,value);
+        }
+    }
+
+    public boolean rContains(int value){
+       return rContains(root,value);
+    }
+
+
 }

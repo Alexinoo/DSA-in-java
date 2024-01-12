499c88b42ba53957e4710db2c0e678c2499b38bc
Alex Mwangi <alex@stl-horizon.com>
2024-01-12 14:14:56 +0300

    Pivot Helper method - Rearranges elements less than the pivot on the left and greater on the right - Return the swap index

---
+++ src/algorithms/sorts/quicksort/Pivot.java
@@ -0,0 +1,41 @@
+package algorithms.sorts.quicksort;
+
+import java.util.Arrays;
+
+public class Pivot {
+
+    public static void main(String[] args){
+       int[] myArray = {4,6,1,7,3,2,5};
+
+       int returnedIndex = pivot(myArray,0,myArray.length-1);
+        System.out.println("\nReturned Index :"+returnedIndex);
+
+        System.out.println(Arrays.toString(myArray));
+    }
+
+    private static int pivot(int[] array, int pivotIndex, int endIndex) {
+        int swapIndex = pivotIndex;
+        for(int i = pivotIndex + 1; i < endIndex ; i++){
+            if( array[i] < array[pivotIndex]){
+                swapIndex++;
+                swap(array, swapIndex, i);
+            }
+        }
+        //end result from the above loop 4,1,3,2,6,7,5 -> swapIndex = 3 pivotIndex = 0
+
+        //swap pivotIndex with swapIndex  2,1,3,4,6,7,5
+
+        swap(array ,pivotIndex, swapIndex);
+        return swapIndex;
+    }
+
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+
+
+
+}
